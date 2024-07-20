8b3aac08610ec5789a20aef56eefbd6b7f0f69db
James Call <56280014+jacall2016@users.noreply.github.com>
2024-07-20 01:12:00 -0600

    changed access-control-allow-origin

--- filehandler/views.py
+++ filehandler/views.py
@@ -102,5 +102,5 @@ def get_csrf_token(request):
     csrf_token = get_token(request)
     response = JsonResponse({'csrfToken': csrf_token})
     response["Access-Control-Allow-Credentials"] = "true"
-    response["Access-Control-Allow-Origin"] = "http://127.0.0.1:5500"
+    response["Access-Control-Allow-Origin"] = "http://localhost:3000"
     return response
