647786e18a5edcec9196e2a9cd9832cbc3f1438a
James Call <56280014+jacall2016@users.noreply.github.com>
2024-07-19 18:59:26 -0600

    cors_all origins true

--- demo/settings.py
+++ demo/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
