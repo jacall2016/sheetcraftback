795250f1d816b70dbd03a6e8f9e5e4b3827c52ff
James Call <56280014+jacall2016@users.noreply.github.com>
2024-07-19 20:59:41 -0600

    allow all origins true

--- demo/settings.py
+++ demo/settings.py
@@ -51,14 +51,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-     'http://127.0.0.1:5500',
-     'http://localhost:3000',
-     'http://127.0.0.1:3000',
-     'https://sheetcraft4newtest1.vercel.app',
-]
+#CORS_ALLOWED_ORIGINS = [
+#     'http://127.0.0.1:5500',
+#     'http://localhost:3000',
+#     'http://127.0.0.1:3000',
+#     'https://sheetcraft4newtest1.vercel.app',
+#]
 
 CORS_ALLOW_CREDENTIALS = True
 
