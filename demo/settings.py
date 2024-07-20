5565dd30ab937d99a93552a5da00d1b6105019b5
James Call <56280014+jacall2016@users.noreply.github.com>
2024-07-20 16:40:18 -0600

    updated settings to try to resolve session issues

--- demo/settings.py
+++ demo/settings.py
@@ -51,7 +51,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# Session settings
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+SESSION_COOKIE_AGE = 1209600  # 2 weeks in seconds
+SESSION_SAVE_EVERY_REQUEST = True
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 
 CORS_ALLOW_ALL_ORIGINS = True
 
